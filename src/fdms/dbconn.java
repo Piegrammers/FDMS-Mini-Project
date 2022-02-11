40ebd960bba712989837e6feeb3787afc5fe8cd7
Gokul P V <dwarakagokul@gmail.com>
2022-02-11 20:45:01 +0530

    bconn class

---
+++ src/fdms/dbconn.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fdms;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Gokul P V
+ */
+public class dbconn {
+    public Connection conn=null;
+    public Statement stmt=null;
+    public dbconn(){
+        try {
+            String driverName="oracle.jdbc.driver.OracleDriver";
+            Class.forName(driverName);
+            String serverName="localhost";
+            String serverPort="1521";
+            String sid="orcl";
+            String url="jdbc:oracle:thin:@"+serverName+":"+serverPort+":"+sid;
+            String username="scott";
+            String password="tiger";
+            conn=DriverManager.getConnection(url,username,password);
+            stmt=conn.createStatement();
+        }
+        catch(ClassNotFoundException e){
+            System.out.println("Driver not found:"+e.getMessage());
+        }
+        catch(SQLException e) {
+            System.out.println("Could not connect to database."+e.getMessage());
+        }
+    }
+    public void end() throws SQLException
+    {
+        stmt.close();
+        conn.close();
+    }
+}
